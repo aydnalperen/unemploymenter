46c3ea2ac42784741bccd6c46348ace5bc7968a7
Alperen Aydın <aydina19@itu.edu.tr>
2022-10-13 20:37:30 +0300

    add navbar and footer

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,23 @@
 const Footer = ()=>{
     return (
         <>
-            <footer>
-                <p>alper</p>
+            <footer class="p-4  shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-300">
+                <span class="text-sm text-black sm:text-center">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+                </span>
+                <ul class="flex flex-wrap items-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
             </footer>
         </>
     )
--- components/navbar.js
+++ components/navbar.js
@@ -3,10 +3,12 @@ const Navbar = ()=>{
     return(
         <>
         <nav className="p-3 bg-gray-300 border-gray-200">
-            <a href="#" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-10" alt="UnEmploymenter Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-black">UnEmploymenter</span>
-            </a>
+            <div className="flex items-center justify-center">
+                <a href="#" class="flex items-center shrink-0" >
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-10" alt="UnEmploymenter Logo" />
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-black">UnEmploymentER</span>
+                </a>       
+            </div> 
         </nav>
         </>
     );
