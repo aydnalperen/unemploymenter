224a38548dbebc26cccfb28d997da3d87cab8bd0
Alperen Aydın <aydina19@itu.edu.tr>
2022-12-14 13:05:58 +0300

    bugfix

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,11 @@
-
+import Link from "next/dist/client/link"
 const Footer = ()=>{
     return (
         <>
             <footer className="p-4  shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-300">
-                <span className="text-sm text-black sm:text-center">© 2022 <a href="/" className="hover:underline">Statisticer</a>. All Rights Reserved.
+                <span className="text-sm text-black sm:text-center">© 2022 <Link href="/" className="hover:underline">Statisticer</Link>. All Rights Reserved.
                 </span>
-                {/* <ul className="flex flex-wrap items-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-                    </li>
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline">Contact</a>
-                    </li>
-                </ul> */}
+                
             </footer>
         </>
     )
