2232579cf3bb59e17e8057c324e75acdaff17cf1
Alperen Aydın <aydina19@itu.edu.tr>
2022-12-14 13:03:39 +0300

    bugfix

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 
 const Navbar = ()=>{
     return(
         <>
         <nav className="p-3 bg-gray-300 border-gray-200 sm:px-4 py-2.5">
             <div className="flex items-center justify-center">
-                <a href="#" className="flex items-center " >
+                <Link href="/" className="flex items-center " >
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">StatisticER</span>
-                </a>       
+                </Link>       
             </div> 
         </nav>
         </>
