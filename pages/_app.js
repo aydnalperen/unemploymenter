d296dbbf8881f15d44c454525bfacafa3e8e0cda
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-07 20:23:28 +0300

    add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>StatisticER</title>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="statistics, inflation, inflation in usa, inflation germany, inflation europe, inflation usa" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
