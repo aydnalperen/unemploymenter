c44c8f5f60a4374adf795bfca8c5b9491160ad1c
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-07 20:44:54 +0300

    add robot tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,8 @@ function MyApp({ Component, pageProps }) {
         <meta charset="UTF-8" />
         <meta name="keywords" content="statistics, inflation, inflation in usa, inflation germany, inflation europe, inflation usa" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="all" />
+        <meta name="googlebot" content="all" />
       </Head>
       <Component {...pageProps} />
     </>
