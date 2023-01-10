5c39d0226e5b9eba33e21adb9c8db6ff51599437
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-10 22:59:51 +0300

    add logo

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
 const Navbar = ()=>{
     return(
         <>
         <nav className="p-3 bg-gray-300 border-gray-200 sm:px-4 py-2.5">
             <div className="flex items-center justify-center">
                 <Link href="/" className="flex items-center " >
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">StatisticER</span>
+                    <Image 
+                        src="/logo.png"
+                        width={200}
+                        height={200}
+                        alt="logo"
+                    />
                 </Link>       
             </div> 
         </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>StatisticER</title>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="keywords" content="statistics, inflation, inflation in usa, inflation germany, inflation europe, inflation usa" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="all" />
