023146b5b2cbeaadb526d152ca81c400cfe00d07
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-10 23:00:29 +0300

    change logo size

--- components/navbar.js
+++ components/navbar.js
@@ -8,8 +8,8 @@ const Navbar = ()=>{
                 <Link href="/" className="flex items-center " >
                     <Image 
                         src="/logo.png"
-                        width={200}
-                        height={200}
+                        width={220}
+                        height={220}
                         alt="logo"
                     />
                 </Link>       
