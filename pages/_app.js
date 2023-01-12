ec37dec299965b7de4dd6e1b284be67a8f0316f2
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-12 19:57:37 +0300

    update keywords

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>StatisticER</title>
         <meta charSet="UTF-8" />
-        <meta name="keywords" content="statistics, inflation, inflation in usa, inflation germany, inflation europe, inflation usa" />
+        <meta name="keywords" 
+        content="statistics, statisticer, inflation, inflation in usa, inflation germany, inflation europe, inflation usa,world bank data,
+        world bank statistics, world bank api,umemployment usa, united states statistics, human development index, statistics by year,
+        
+        " />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="all" />
         <meta name="googlebot" content="all" />
