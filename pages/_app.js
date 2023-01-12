ae16b45a9292e7cc6a169d25f819803691ed8379
Alperen Aydın <aydina19@itu.edu.tr>
2023-01-12 20:22:35 +0300

    add adsense

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,16 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>StatisticER</title>
         <meta charSet="UTF-8" />
-        <meta name="keywords" 
-        content="statistics, statisticer, inflation, inflation in usa, inflation germany, inflation europe, inflation usa,world bank data,
+        <meta name="keywords"
+          content="statistics, statisticer, inflation, inflation in usa, inflation germany, inflation europe, inflation usa,world bank data,
         world bank statistics, world bank api,umemployment usa, united states statistics, human development index, statistics by year,
         stat graphs, statistics graphics, simple statistics
         " />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="all" />
         <meta name="googlebot" content="all" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8252929364939554"
+     crossorigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
     </>
